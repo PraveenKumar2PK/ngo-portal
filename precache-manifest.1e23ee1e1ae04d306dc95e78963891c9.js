6c68c010afdfb7fadc6b7184277f1ddd33571b50
Praveen kumar <erodepraveen94@gmail.com>
2019-10-25 00:32:36 +0530

    Updates

---
+++ precache-manifest.1e23ee1e1ae04d306dc95e78963891c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63f0e05f58eb06514cf5231880c5f7f",
+    "url": "/ngo-front/index.html"
+  },
+  {
+    "revision": "484a2686ccf1355b6f21",
+    "url": "/ngo-front/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "6b2f83ad23f0fab505b7",
+    "url": "/ngo-front/static/css/main.c5bdcc5d.chunk.css"
+  },
+  {
+    "revision": "484a2686ccf1355b6f21",
+    "url": "/ngo-front/static/js/2.a8be0fee.chunk.js"
+  },
+  {
+    "revision": "6b2f83ad23f0fab505b7",
+    "url": "/ngo-front/static/js/main.788924b8.chunk.js"
+  },
+  {
+    "revision": "5fd661cb71aa06b3c8f8",
+    "url": "/ngo-front/static/js/runtime-main.79c30d1f.js"
+  },
+  {
+    "revision": "ec057ae5db1154aff80e7355d67a295f",
+    "url": "/ngo-front/static/media/header-theme-1.ec057ae5.jpg"
+  }
+]);
